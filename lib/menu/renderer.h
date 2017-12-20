9934a96a3f984db837e57e4acfd00692a859364b
punky <punky@tromblon>
2017-12-20 16:44:38 +0100

    rewrite renderer with functor typedefs

--- lib/menu/renderer.h
+++ lib/menu/renderer.h
@@ -3,29 +3,36 @@
 
 namespace Menu
 {
+    // text, width, height
+    typedef void (*GETTEXTSIZECHAR)(const char *, uint8_t*, uint8_t*);
+    // text, width, height
+    typedef void (*GETTEXTSIZEFSH)(const __FlashStringHelper *, uint8_t*, uint8_t*);
+    // x, y, text
+    typedef void (*DRAWTEXTCHAR)(uint8_t, uint8_t, const char *);
+    // x, y, text
+    typedef void (*DRAWTEXTFSH)(uint8_t, uint8_t, const __FlashStringHelper *);
+    // x0, y0, x1, y1 
+    typedef void (*DRAWLINE)(uint8_t, uint8_t, uint8_t, uint8_t);
+    // x, y, w, h
+    typedef void (*DRAWRECT)(uint8_t, uint8_t, uint8_t, uint8_t);
+    // x, y, w, h
+    typedef void (*FILLRECT)(uint8_t, uint8_t, uint8_t, uint8_t);
 
     static const uint8_t text_spacing = 1;
     static const uint8_t text_margin = 2;
 
 struct Renderer
 {
-    Renderer(int screenWidth, int screenHeight) 
-    : screenWidth(screenWidth), screenHeight(screenHeight) { }
+    Renderer(int screenWidth, int screenHeight)
+    : screenWidth(screenWidth), screenHeight(screenHeight) {}
 
-    // text, width, height
-    void (*getTextSizeChar)(const char *, uint8_t*, uint8_t*);
-        // text, width, height
-    void (*getTextSizeFSH)(const __FlashStringHelper*, uint8_t*, uint8_t*);
-    // x, y, text
-    void (*drawTextChar)(uint8_t, uint8_t, const char *);
-    // x, y, text
-    void (*drawTextFSH)(uint8_t, uint8_t, const __FlashStringHelper *);
-    // x0, y0, x1, y1 
-    void (*drawLine)(uint8_t, uint8_t, uint8_t, uint8_t);
-    // x, y, w, h
-    void (*drawRect)(uint8_t, uint8_t, uint8_t, uint8_t);
-    // x, y, w, h
-    void (*fillRect)(uint8_t, uint8_t, uint8_t, uint8_t);
+    GETTEXTSIZECHAR getTextSizeChar;
+    GETTEXTSIZEFSH getTextSizeFSH;
+    DRAWTEXTCHAR drawTextChar;
+    DRAWTEXTFSH drawTextFSH;
+    DRAWLINE drawLine;
+    DRAWRECT drawRect;
+    FILLRECT fillRect;
 
     uint8_t screenWidth;
     uint8_t screenHeight;
