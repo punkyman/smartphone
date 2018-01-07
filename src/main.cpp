c3639b968cf1496b09e1a047a2c35e7285dadb10
punky <punky@tromblon>
2018-01-07 10:13:38 +0100

    declare debug define, allows to claim serial output code

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@
 #include "hardware.h"
 #include "phone_menu.h"
 
+#define DEBUG
+
 // tick every 66 ms, that makes it 15fps
 #define TIME_FRAME 66
 
 PhoneMenu menu;
 
 void setup(void) {
+#if defined(DEBUG)
   Serial.begin(9600);
+#endif
 
   Hardware::setup();
 }
@@ -22,13 +26,16 @@ void loop(void) {
 
   Hardware::update();
   menu.update();
-  
+
+#if defined(DEBUG)
   unsigned long endUpdateTime = micros();
+#endif
 
   menu.draw();
 
-unsigned long endTime = micros();
-  
+  unsigned long endTime = micros();
+
+#if defined(DEBUG)
   Serial.print(F("Update time : "));
   Serial.println(endUpdateTime - startTime);
 
@@ -37,6 +44,7 @@ unsigned long endTime = micros();
 
   Serial.print(F("Free memory : "));
   Serial.println(freeMemory());
+#endif
 
   int timeframe = TIME_FRAME - ((endTime - startTime) / 1000);
   if(timeframe > 0)
