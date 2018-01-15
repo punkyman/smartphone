749f2a2c651509f7d4107adfbc60cbce65770e79
punky <punky@tromblon>
2018-01-15 13:16:17 +0100

    clarify some things

--- src/hardware_config.h
+++ src/hardware_config.h
@@ -21,8 +21,9 @@
 // Displays
 ///////////////////////////////////////////////////////////////////////////////
 
-#define HARDWARE_USE_SH1106
-#define HARDWARE_USE_PCD8544
+// This has to be done in adafruit_wrapper for now ...
+//#define HARDWARE_USE_SH1106
+//#define HARDWARE_USE_PCD8544
 
 ///////////////////////////////////////////////////////////////////////////////
 // I2C addresses
@@ -39,37 +40,28 @@
 // Pinout
 ///////////////////////////////////////////////////////////////////////////////
 
-// This module is accessed through SPI, it uses the default pinout of the SPI implementation
+// This module is accessed through SPI, it uses the pinout of the SPI implementation
+// in adafruit_wrapper.cpp : SCK : 13, MOSI : 11, DC : 7, CS : 8, RST : 9
 // HARDWARE_USE_PCD8544
 
-// These modules are accessed through I2C, so they use the default pinout of Wire library
+// These modules are accessed through I2C, they use the default pinout of Wire library
+// on arduino mini : SDA : A4, SCL : A5
 // HARDWARE_ENABLE_SENSORS
 // HARDWARE_ENABLE_RTC
 // HARDWARE_USE_SH1106
 
-//#if defined(HARDWARE_ENABLE_GPS)
 #define GPS_SS_RX 3
 #define GPS_SS_TX 4
-//#endif
 
-//#if defined(HARDWARE_ENABLE_GSM)
 #define GSM_SS_RX 5
 #define GSM_SS_TX 6
-//#endif
 
-//#if defined(HARDWARE_ENABLE_BATTERY)
 #define BATTERY_ANALOG_READ A2
-//#endif
 
-//#if defined(HARDWARE_USE_JOYSTICK)
 #define JOYSTICK_ANALOG_X A0
 #define JOYSTICK_ANALOG_Y A1
 #define JOYSTICK_DIGITAL_SWITCH 2
-//#endif
 
-//#if defined(HARDWARE_USE_ENCODER)
-#define ENCODER_DIGITAL_CLOCK 2 // needs to be on an interrupt compatible pin
+#define ENCODER_DIGITAL_CLOCK 2 // needs to be on an interrupt compatible pin, on arduino mini : 2 or 3
 #define ENCODER_DIGITAL_DATA 4
 #define ENCODER_DIGITAL_SWITCH 5
-//#endif
-
