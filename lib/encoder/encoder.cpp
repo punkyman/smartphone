41aa610bd5b9656ccad9ff3051a8addce7666099
punky <punky@tromblon>
2018-01-15 10:04:43 +0100

    implementation of the encoder with interrupts

--- lib/encoder/encoder.cpp
+++ lib/encoder/encoder.cpp
@@ -1,2 +1,39 @@
 #include "encoder.h"
 
+volatile int8_t encoder_scrolls;
+bool encoder_switch;
+
+void encoder_interrupt()
+{
+    if(digitalRead(ENCODER_DIGITAL_DATA))
+    {
+        --encoder_scrolls; // CCW
+    }
+    else
+    {
+        ++encoder_scrolls; // CW
+    }
+}
+
+void init()
+{
+    encoder_scrolls = 0;
+    encoder_switch = false;
+
+    pinMode(ENCODER_DIGITAL_CLOCK, INPUT);
+    pinMode(ENCODER_DIGITAL_DATA, INPUT);
+    pinMode(ENCODER_DIGITAL_SWITCH, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(ENCODER_DIGITAL_CLOCK), encoder_interrupt, RISING);
+}
+
+void encoder_clear_readings()
+{
+    encoder_scrolls = 0;
+}
+
+void encoder_read_values()
+{
+    encoder_switch = digitalRead(ENCODER_DIGITAL_SWITCH) != HIGH;
+}
+
--- lib/encoder/encoder.h
+++ lib/encoder/encoder.h
@@ -1,3 +1,13 @@
 #pragma once
+#include <Arduino.h>
 
+#define ENCODER_DIGITAL_CLOCK 2 // needs to be on an interrupt compatible pin
+#define ENCODER_DIGITAL_DATA 4
+#define ENCODER_DIGITAL_SWITCH 5
 
+extern volatile int8_t encoder_scrolls;
+extern bool encoder_switch;
+
+void encoder_init();
+void encoder_clear_readings();
+void encoder_read_values();
