245c36b3c95e018005a9184ea3c0ae9d523750fd
punky <punky@tromblon>
2018-01-17 22:37:10 +0100

    prototype of a non blocking way to read at responses

--- lib/gsm/src/ATSerial.cpp
+++ lib/gsm/src/ATSerial.cpp
@@ -5,22 +5,39 @@ ATSerial::ATSerial(uint8_t receivePin, uint8_t transmitPin)
 {
 }
 
-String ATSerial::at_read_response()
+void ATSerial::serial_update()
+{
+    while(available())
+    {
+        buffer += read();
+    }
+}
+
+bool ATSerial::serial_at_response_available()
+{
+    const char* ptr = buffer.c_str();
+    uint8_t nb = 0;
+    while(*ptr != 0)
+    {
+        if(*ptr == '\n')
+        {
+            ++nb;
+            if(nb == 2)
+                return true;
+        }
+        ++ptr;
+    }
+    return false;
+}
+
+String ATSerial::serial_at_read_response()
 {
   String ret;
 
   // AT response is supposed to follow format : <CR><LF>response<CR><LF>
-  int c = timedRead();
-  while (c != '\n') // <LF>
-  {
-    c = timedRead();
-  }
-  while (c != '\r') // <CR>
-  {
-    ret += (char)c;
-    c = timedRead();
-  }
-  c = timedRead(); // to remove LF from stream
+  int index = buffer.indexOf('\n', 2);
+  ret = buffer.substring(0, index);
+  buffer.remove(index, buffer.length() - index);
 
   return ret;
 }
--- lib/gsm/src/ATSerial.h
+++ lib/gsm/src/ATSerial.h
@@ -6,11 +6,15 @@ struct ATSerial : SoftwareSerial
 {   
     ATSerial(uint8_t receivePin, uint8_t transmitPin);
 
-    String at_read_response();
+    void serial_update();
+    bool serial_at_response_available();
+    String serial_at_read_response();
 
     bool at_get_ok();
     bool at_get_tag(const char* tag, uint8_t* value);
 
     bool at_stop_messages();
     bool at_resume_messages();
+
+    String buffer;
 };
